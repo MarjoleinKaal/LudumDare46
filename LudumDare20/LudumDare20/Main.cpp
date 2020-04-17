abd7714b25371490498c9f4f022b535880080d1f
Marjolein <marjoleinkaal@gmail.com>
2020-04-17 19:49:30 +0200

    [Add] Initial project commit

---
+++ LudumDare20/LudumDare20/Main.cpp
@@ -0,0 +1,29 @@
+﻿// LudumDare20.cpp : Defines the entry point for the application.
+//
+
+#include "Main.h"
+#include "raylib.h"
+
+using namespace std;
+
+int main()
+{
+	const int screenWidth = 1600;
+	const int screenHeight = 900;
+
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText("Yeeh a window", 190, 200, 60, LIGHTGRAY);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
