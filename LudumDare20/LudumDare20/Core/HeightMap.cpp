205aad551eae0e483d096f54679a94940a659ad4
Marjolein <marjoleinkaal@gmail.com>
2020-04-18 12:51:23 +0200

    [Add] Glob statement + heightmap files

---
+++ LudumDare20/LudumDare20/Core/HeightMap.cpp
@@ -0,0 +1,37 @@
+#include "HeightMap.h"
+#include "Raylib.h"
+
+HeightMap::HeightMap(const char* path, Vector3 position, Vector3 size)
+{
+	Image image = LoadImage(path);
+	texture = LoadTextureFromImage(image);
+
+	Mesh mesh = GenMeshHeightmap(image, size);
+	model = LoadModelFromMesh(mesh);
+
+	model.materials[0].maps[MAP_DIFFUSE].texture = texture;
+	mapPosition = position;
+
+	UnloadImage(image)
+}
+
+HeightMap::~HeightMap()
+{
+}
+
+
+Vector3 HeightMap::GetPosition()
+{
+	return mapPosition;
+}
+
+void HeightMap::DrawHeightMap()
+{
+	DrawModel(model, mapPosition, 1.0f, RED);
+}
+
+void HeightMap::UnloadHeightMap()
+{
+	UnloadTexture(texture);
+	UnloadModel(model);
+}
---
+++ LudumDare20/LudumDare20/Core/HeightMap.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Raylib.h"
+#include "BasicTypes.h"
+
+class HeightMap
+{
+public:
+	HeightMap(const char* path, Vector3 position, Vector3 size);
+	~HeightMap();
+
+	Vector3 GetPosition();
+
+	void DrawHeightMap();
+	void UnloadHeightMap();
+private:
+	Vector3 mapPosition;
+	Model model;
+	Texture2D texture;
+};
+
